d84d259b936284c9dc19deebd56fa0d62d70de8a
Petrovaes11122024 <pesfsb@gmail.ru>
2024-12-13 17:57:47 +1000

    Update hello_git py. Add abiut VCS

--- hello_git.py
+++ hello_git.py
@@ -3,5 +3,15 @@ print('Hello GIT')
 print("Индексация - это' важно")
 
 print('Create new branch testing')
-for num in range(10)
-    print(num,end=2)
+import os
+print('Hello, Git!')
+
+print('Indexing is important!')
+
+for num in range(10):
+        print(num,end=' ')
+
+print(f'\nYour current directory: {os.getcwd()}')
+
+s = 'Version Control System'
+print(f'{s} - length is {len(s)} characters')
